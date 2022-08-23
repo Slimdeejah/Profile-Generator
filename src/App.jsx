548c0c07bfdd2f6fdd77da5d1e0779e409ccf9d4
Deola <abolanlethany@gmail.com>
2022-08-23 16:34:53 +0100

    profile

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export const App = () => {
+  const [state, setState] = useState([]);
+
+  const ProfileData = async () => {
+    await axios
+      .get("https://randomuser.me/api")
+      .then((response) => {
+        setState(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => error);
+  };
+
+  useEffect(() => {
+    ProfileData();
+  }, []);
+
+  return (
+    <>
+      {state.results?.map((item, index) => (
+        <div className="card-wrapper" key={index}>
+          {/* <div className="card-wrapper-cover">
+            Hover Me
+          </div> */}
+          <div className="card-header"></div>
+          <img src={item.picture?.large} alt="pic" />
+          <h1>
+            {item.name?.first} {item.name?.last}
+          </h1>
+          <p>{item.email}</p>
+          <span>{item.location?.timezone?.description}</span>
+          <button onClick={() => ProfileData()}>Next Profile</button>
+        </div>
+      ))}
+    </>
+  );
+};
